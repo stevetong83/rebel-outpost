a326d10714a3097d18e1f73e7a1505557e7a7273
Stephen Tong <stevetong83@hotmail.com>
2013-08-21 14:25:00 -0700

    added pages

---
+++ app/models/message.rb
@@ -0,0 +1,4 @@
+class Message
+  include Mongoid::Document
+  field :name, type: String
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ RebelOutpost::Application.routes.draw do
 
   root to: 'pages#home'
 
+  resources :messages, only: [:new, :create]
+
+  get 'contact',  to: 'messages#new'
+  get 'services', to: 'pages#services'
+  get 'process',  to: 'pages#process'
 end
